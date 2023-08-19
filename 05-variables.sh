0195125e0d1403cfdddbf3ff2af18930b8cdaf66
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-19 12:39:45 -0400

    helloworld

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,13 +2,13 @@
 
 echo "please enter your username"
 
-read USERNAME 
+read -s USERNAME 
 
 echo " username entered is: $USERNAME"
 
 echo "please enter your password"
 
-read PASSWORD
+read -s PASSWORD
 
 echo " password entered is: $PASSWORD"
 
