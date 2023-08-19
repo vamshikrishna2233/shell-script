dc29fe616d48574da9acc697afe87abeab40f980
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-19 12:24:25 -0400

    helloworld

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello world, i am learning devops with aws"
+
+person1=$1
+person2=$2
+
+echo "$person1 : Hi $person2 , good morning"
+echo "$person2 : good morning $person1"
+echo "$person1 : how are you $person2"
+echo "$person2 : i am good , how are you $person1"
