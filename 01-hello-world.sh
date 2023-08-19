478012ec78462f675d3ebd45accf6ea8cb748220
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-19 12:09:54 -0400

    helloworld

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello world, i am learning devops with aws"
+
+echo "ramesh : Hi suresh , good morning"
+echo "suresh : good morning ramesh"
+echo "ramesh : how are you suresh"
+echo "suresh : i am good , how are you ramesh"
