a1e85f4715199c910a973589392c45ffeae4969a
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-20 06:18:57 -0400

    added condition

--- 12-loops.sh
+++ 12-loops.sh
@@ -5,7 +5,7 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then 
-    echo "$R ERROR: please run this script with root access $N"
+    echo -e "$R ERROR: please run this script with root access $N"
     exit 1
 fi
 
