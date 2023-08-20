5f735135b23d28e143e847ca720176a6bbf21f56
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-20 03:27:21 -0400

    added condition

--- 09-functions.sh
+++ 09-functions.sh
@@ -6,14 +6,18 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE(){
     #1------ it will receive the argument
     if [ $? -ne 0 ]
     then 
-        echo "$2 ..... Failure"
+        echo "$2 ..... $R Failure $N"
         exit 1
     else 
-        echo "$2.....success"
+        echo "$2.....$G success $N"
     fi
 }
 
