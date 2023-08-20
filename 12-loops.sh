5965b0cefdb780c20961c73f5f4dd7e2da7f9682
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-20 07:30:53 -0400

    added condition

--- 12-loops.sh
+++ 12-loops.sh
@@ -30,7 +30,7 @@ VALIDATE(){
 for i in $@
 do 
     yum list installed $i &>>$LOGFILE
-    if [$? -ne 0 ]
+    if [ $? -ne 0 ]
     then
         echo "$i is not installed, lets install it"
         yum install $i -y &>> $LOGFILE
