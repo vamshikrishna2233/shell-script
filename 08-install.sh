52c133c49d277c33e7d949f153cb040eaf7b72f5
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-20 01:45:42 -0400

    added condition

---
+++ 08-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#our program goal is to install mysql
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Error : please run this script with root access"
+else 
+    echo "INFO : you are root user"
+fi
+
+yum install git -y
